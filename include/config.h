003bebe85da7749ac6638b61f1e2ff8a4322d97f
qiu <qiujieting@163.com>
2022-04-06 16:01:53 +0800

    增加自测环境的IP地址和MAC地址

--- include/config.h
+++ include/config.h
@@ -8,14 +8,26 @@
 #define UDP
 
 
+#ifdef TEST
 #define NET_IF_IP    \
     {                   \
-        192, 168, 56, 2 \
+        192, 168, 163, 103 \
+    } //测试用网卡ip地址
+#define NET_IF_MAC                      \
+    {                                      \
+        0x11, 0x22, 0x33, 0x44, 0x55, 0x66 \
+    } //测试用网卡mac地址
+#else
+#define NET_IF_IP    \
+    {                   \
+        XX, XX, XX, XX \
     } //自定义网卡ip地址
 #define NET_IF_MAC                      \
     {                                      \
         0x00, 0x11, 0x22, 0x33, 0x44, 0x55 \
     } //自定义网卡mac地址
+#endif 
+
 
 
 #define ETHERNET_MAX_TRANSPORT_UNIT 1500 //以太网最大传输单元
